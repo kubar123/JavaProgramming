fa4f58d383d23a1ab1465e3d5015d8eadb6bca62
kubar123 <kubar123@hotmail.com>
2015-02-19 18:58:58 +1100

    added q12

---
+++ 2015/ch13/Player/Child.java
@@ -0,0 +1,18 @@
+/*
+ * Creative Commons Attribution-NonCommercial 3.0 Australia license ( CC BY-NC 3.0 AU)
+ * http://creativecommons.org/licenses/by-nc/3.0/au/legalcode  * 
+ */
+package ch11;
+
+/**
+ *
+ * @author Jakub Rybicki <lansoftprogramming.com>
+ */
+public class Child implements Player{
+    
+    @Override
+    public void play() {
+        System.out.println("child's play");
+    }
+    
+}
---
+++ 2015/ch13/Player/UsePlayer.java
@@ -0,0 +1,22 @@
+/*
+ * Creative Commons Attribution-NonCommercial 3.0 Australia license ( CC BY-NC 3.0 AU)
+ * http://creativecommons.org/licenses/by-nc/3.0/au/legalcode  * 
+ */
+package ch11;
+
+
+
+/**
+ *
+ * @author Jakub Rybicki <lansoftprogramming.com>
+ */
+public class UsePlayer {
+    public static void main(String[] args){
+        Child c1=new Child();
+        Musician m1=new Musician();
+        Actor a1=new Actor();
+        c1.play();
+        m1.play();
+        a1.play();
+        }
+}
